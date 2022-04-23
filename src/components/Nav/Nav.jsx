dd85d82e0b70eb35c1bad61e13801049220e23c9
KhiaJEO <khiajeo@gmail.com>
2022-04-23 11:47:55 +0800

    Why is my heroku app not loading?
    :(

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Nav() {
-    return (
-        <nav>
-            <Link to="/">HomePage</Link>
-            <Link to="/project">ProjectPage</Link>
-        </nav>
-    );
+  return (
+    <nav>
+      <Link to="/">HomePage</Link>
+    </nav>
+  );
 }
 
 export default Nav;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // Components
 import ProjectCard from "../components/ProjectCard/ProjectCard";
@@ -7,18 +7,34 @@ import ProjectCard from "../components/ProjectCard/ProjectCard";
 import { allProjects } from "../data";
 
 function HomePage() {
-    return (
-        <div id="project-list">
-            {allProjects.map((projectData, key) => {
-                return <ProjectCard 
-                    key={`project-${projectData.id}`} 
-                    projectData={projectData}
-                />;
-            })}
-        </div>
-    );
+  // States
+  const [projectList, setProjectList] = useState([]);
+
+  // Action & Helpers
+  // useeffect runs the data into the state to re-render the project list on the homepage.
+  // The array [] means it will only run once, but you can values into [] to run again and again
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}projects`)
+      .then((results) => {
+        return results.json();
+      })
+      .then((data) => {
+        setProjectList(data);
+      });
+  }, []);
+
+  return (
+    <div id="project-list">
+      {projectList.map((projectData, key) => {
+        return (
+          <ProjectCard
+            key={`project-${projectData.id}`}
+            projectData={projectData}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
-
-// Talk to mentors abouts line 12 - 14
 export default HomePage;
